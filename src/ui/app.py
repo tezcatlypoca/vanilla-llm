f45ddce6a23f873b6590495c9c0562849f5ceec9
Samsân <mrkimbou21972@gmail.com>
2025-12-01 22:37:45 +0100

    ADD: code basique pour une fenêtre tkinter

---
+++ src/ui/app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+# Set le titre de la fenêtre
+root.title("Vanilla MLP")
+# Set la taille de la fenêtre
+root.geometry('500x350')
+
+main_frame = ttk.Frame(root)
+
+root.mainloop()
